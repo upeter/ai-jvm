c866cb39de912ec8fb0de513831a6df5a11588aa
upeter <peter.urs@gmail.com>
2025-05-14 09:20:41 +0200

    adjust ingestion

--- langchain4j-ktor/src/main/kotlin/dev/example/demo/Routes.kt
+++ langchain4j-ktor/src/main/kotlin/dev/example/demo/Routes.kt
@@ -14,7 +14,21 @@ fun Route.aiRoutes(
     streamingChatLanguageModel: StreamingChatLanguageModel) {
 
     get("/ai/ask") {
-        TODO()
+        val message = call.request.queryParameters["message"] ?: return@get missingParameterReply()
+        val response = chatLanguageModel.generate(message)
+        call.respondText(response)
+
+    }
+
+    get("/ai/ask/stream") {
+        val message = call.request.queryParameters["message"] ?: return@get missingParameterReply()
+        val responseFlow = streamingChatLanguageModel.generateFlow(message)
+        call.respondTextWriter {
+            responseFlow.collect { chunk ->
+                write(chunk)
+                flush()
+            }
+        }
     }
 
 
