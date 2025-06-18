a2853e5b5857e1eed954d6eede0685e1bdec6378
upeter <peter.urs@gmail.com>
2025-06-18 16:11:27 +0200

    upgrade mcp version

--- kotlin-mcp-server/build.gradle.kts
+++ kotlin-mcp-server/build.gradle.kts
@@ -18,7 +18,7 @@ group = "org.example"
 version = "0.1.0"
 
 
-val mcpVersion = "0.5.0"
+val mcpVersion = "0.6.0"
 val slf4jVersion = "2.0.9"
 val ktorVersion = "3.1.1"
 dependencies {
@@ -26,7 +26,7 @@ dependencies {
     implementation("org.slf4j:slf4j-nop:$slf4jVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-//    implementation("net.pwall.json:json-kotlin-schema:0.56")    // build.gradle.kts
+    implementation("net.pwall.json:json-kotlin-schema:0.56")    // build.gradle.kts
     //implementation("com.github.Ricky12Awesome:json-schema-serialization:0.6.6")
 testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
